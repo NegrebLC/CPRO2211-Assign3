22813f8a711242cf057205b656da814bbe03a634
NegrebLC <BergCu2001@gmail.com>
2024-03-15 01:05:21 -0600

    Add project files.

---
+++ CPRO2211-Assign3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CPRO2211_Assign3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
